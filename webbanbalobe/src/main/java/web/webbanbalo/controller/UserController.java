f4fdc4ee53d3672eb260045cf28ea6ea1ef93bb5
nganguyen2022 <20130077@st.hcmuaf.edu.vn>
2024-05-15 22:02:22 +0700

    back-end: update personal information

---
+++ webbanbalobe/src/main/java/web/webbanbalo/controller/UserController.java
@@ -0,0 +1,42 @@
+package web.webbanbalo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import web.webbanbalo.entity.User;
+import web.webbanbalo.repository.UserRepository;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @PutMapping("/{userId}")
+    public ResponseEntity<User> updateUser(
+            @PathVariable int userId,
+            @RequestBody User userToUpdate) {
+
+        Optional<User> optionalUser = userRepository.findById(userId);
+        if (optionalUser.isPresent()) {
+            User existingUser = optionalUser.get();
+            existingUser.setFullName(userToUpdate.getFullName());
+            existingUser.setPhone(userToUpdate.getPhone());
+            existingUser.setEmail(userToUpdate.getEmail());
+            existingUser.setAddress(userToUpdate.getAddress());
+
+            User updatedUser = userRepository.save(existingUser);
+            return ResponseEntity.ok(updatedUser);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
