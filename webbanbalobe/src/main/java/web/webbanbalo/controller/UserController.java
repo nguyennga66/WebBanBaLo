ae1890eb414f9f45c01be1d2a06af611ecb2a4d2
nganguyen2022 <20130077@st.hcmuaf.edu.vn>
2024-05-16 21:39:56 +0700

    back-end: cập nhật chức năng update infor for user

--- webbanbalobe/src/main/java/web/webbanbalo/controller/UserController.java
+++ webbanbalobe/src/main/java/web/webbanbalo/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import web.webbanbalo.entity.User;
 import web.webbanbalo.repository.UserRepository;
 
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -31,19 +32,28 @@ public class UserController {
     public ResponseEntity<User> updateUser(
             @PathVariable int userId,
             @RequestBody User userToUpdate) {
+        try {
+            Optional<User> optionalUser = userRepository.findById(userId);
+            if (optionalUser.isPresent()) {
+                User existingUser = optionalUser.get();
+                existingUser.setFullName(userToUpdate.getFullName());
+                existingUser.setPhone(userToUpdate.getPhone());
+                existingUser.setEmail(userToUpdate.getEmail());
+                existingUser.setAddress(userToUpdate.getAddress());
 
-        Optional<User> optionalUser = userRepository.findById(userId);
-        if (optionalUser.isPresent()) {
-            User existingUser = optionalUser.get();
-            existingUser.setFullName(userToUpdate.getFullName());
-            existingUser.setPhone(userToUpdate.getPhone());
-            existingUser.setEmail(userToUpdate.getEmail());
-            existingUser.setAddress(userToUpdate.getAddress());
+                User updatedUser = userRepository.save(existingUser);
 
-            User updatedUser = userRepository.save(existingUser);
-            return ResponseEntity.ok(updatedUser);
-        } else {
-            return ResponseEntity.notFound().build();
+                if (updatedUser != null){
+                    return ResponseEntity.ok(updatedUser);
+                } else{
+                    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+                }
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } catch(Exception e){
+            System.err.println("Lỗi khi cập nhật thông tin người dùng: " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
 }
